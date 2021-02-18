7662b8d82f6e6f6b36ffafedc7f5204a7a2d0652
McCotter36 <73186298+McCotter36@users.noreply.github.com>
2021-02-18 14:04:13 -0700

    Update Chat.js
    added firebase
    disabled warnings
    remove bubbleprops

--- components/Chat.js
+++ components/Chat.js
@@ -6,13 +6,13 @@ import {
   Button, 
   StyleSheet, 
   Platform, 
-  KeyboardAvoidingView 
+  KeyboardAvoidingView, 
+  LogBox
 } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
-const firebase = require('firebase');
-require('firebase/firestore');
-
-console.disableYellowBox = true;
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 //Chat Component to render chat UI
 class Chat extends React.Component {
@@ -20,10 +20,17 @@ class Chat extends React.Component {
     super(props);
     this.state = {
       messages: [],
-    }
+      uid: 0,
+      user: {
+        _id: '',
+        name: '',
+        avatar: '',
+      },
+      loggedInText: 'Please wait while you are logged in.'
+    };
 
     // firestore configuration 
-    const firebaseConfig = {
+    var firebaseConfig = {
       apiKey: "AIzaSyBgf5rm_JbZKeFpCWTOqnPMyiEenKNedys",
       authDomain: "speak-easy-70b1d.firebaseapp.com",
       projectId: "speak-easy-70b1d",
@@ -38,34 +45,99 @@ class Chat extends React.Component {
       //initialize app using above configuration variable
       firebase.initializeApp(firebaseConfig);
     } 
-    //reference messages
-    // this.referenceChatMessages = null;
+    // Reference Chat messages in firebase
+    this.referenceChatMessages = firebase.firestore().collection("messages");
 
+    // Ignores warning messages specified
+    LogBox.ignoreLogs([
+      'Setting a timer',
+      'undefined',
+    ])
   }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // iterate through each document
+    querySnapshot.forEach((doc) => {
+      //get the QueryDocumentSnapshot's data
+      var data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar,
+        },
+      });
+    });
+    this.setState({ messages });
+  };
+
   addMessage() {
-    this.referenceChatMessages.add(
-
-    )
-
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      _id: message._id,
+      uid: this.state.uid,
+      createdAt: message.createdAt,
+      text: message.text || '',
+      user: message.user,
+      image: message.image || '',
+      location: message.location || null,
+    });
   }
+
   onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessage();
+      }
+    );
   }
 
-  // Properties of the message bubble (right or left)
-  renderBubble(props) {
-    return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          right: {
-            backgroundColor: '#000'
-          }
-        }}
-      />
-    )
+  componentDidMount() {
+    //set title of Chat component to name entered in Start input
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+
+    // Listen for collection changes for current user
+    this.referenceChatMessages = firebase.firestore().collection("messages")
+    // ==== disabled uid in favor of loading all messages for the time being
+    // .where("uid", "==", this.state.uid)
+    ;
+
+    // sign in Anonymously with firebase and listen for Auth State Change
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      // add user uid to state
+      this.setState({
+        uid: user.uid,
+        user: {
+          _id: user.uid,
+          name: name,
+          avatar: 'https://placeimg.com/140.140.any',
+        },  
+        loggedInText: "Welcome",
+        messages: [],
+      });
+      // listen for changes to messages
+      this.unsubscribeChatMessageUser = this.referenceChatMessages
+      .orderBy("createdAt", "desc")
+      .onSnapshot(this.onCollectionUpdate);
+    });
+  }
+
+  componentWillUnmount() {
+    // stops listening for changes
+    this.unsubscribeChatMessageUser();
+    // stops listening for authentication changes
+    this.authUnsubscribe();
   }
 
   render () {
@@ -74,13 +146,17 @@ class Chat extends React.Component {
    
     return (
       <View style={[styles.container, {backgroundColor: color }]}>
+        <Text style={styles.welcome}>{this.state.loggedInText}</Text>
+         <View style={styles.buttonView}>
          <Button
           title="Go to Start"
+          color="purple"
+          style={styles.button}
           //navigates user back to start Component
           onPress={() => this.props.navigation.navigate('Start')}
           />
+          </View>
         <GiftedChat
-        renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
           user={{
@@ -88,62 +164,24 @@ class Chat extends React.Component {
           }}
         />
       {/* android specific fix prevents keyboard from hiding input */} 
-      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height' /> : null }
+      { Platform.OS === 'android' ? (<KeyboardAvoidingView behavior='height' />) : null }
       </View>
     );
   }
-
-  componentDidMount() {
-    //set title of Chat component to name entered in Start input
-    this.referenceChatMessages = firebase.firestore().collection("messages");
-    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
-      if (!user) {
-        firebase.auth().signInAnonymously();
-      }
-      this.setState({
-        uid: user.uid,
-        messages: [],
-      });
-      this.unsubscribe = this.referenceChatMessages
-      .orderBy("createdAt", "desc")
-      .onSnapshot(this.onCollectionUpdate);
-    });
-
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: 'This is a system message',
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
-    })
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-    this.authUnsubscribe();
-
-  }
 }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1, 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  buttonView: {
+    flex: 1,
+    alignItems: 'center',
+  }
 });
 
 export default Chat;
